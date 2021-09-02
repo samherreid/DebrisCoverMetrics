a343732b2678363a0bbf14fa1e63ed0beb1993ee
samherreid <samherreid@gmail.com>
2021-09-01 22:38:47 -0400

    commonCoords fn working

--- .ipynb_checkpoints/commonCoords-checkpoint.py
+++ .ipynb_checkpoints/commonCoords-checkpoint.py
@@ -1,17 +1,22 @@
-if spatialRef != "glacier_shp" and spatialRef != "debris_shp":
-    if not os.path.exists(workspace + "\\debris_raw_commonProjection.tif"):
-        arcpy.Project_management(debris, "dem_raw_commonProjection.tif", spatialRef)
-    if not os.path.exists(workspace + "\\glaciers_commonProjection.shp"):
-        arcpy.Project_management(glaciers, "glaciers_commonProjection.shp", spatialRef)
-if spatialRef == "glacier_shp":
-    spatialRef = arcpy.Describe(glaciers).spatialReference
-    if not os.path.exists(workspace + "\\"+regionName+"_raw_commonProjection.tif"):
-        arcpy.CopyRaster_management(dem, "dem_raw_commonProjection.tif")
-    if not os.path.exists(workspace + "\\glaciers_commonProjection.shp"):
-        arcpy.Project_management(glaciers, "glaciers_commonProjection.shp", spatialRef)
-elif spatialRef == "glaciers":
-    spatialRef = arcpy.Describe(glaciers).spatialReference
-    if not os.path.exists(workspace + "\\glaciers_commonProjection.shp"):
-        arcpy.CopyFeatures_management(glaciers, "glaciers_commonProjection.shp")
-    if not os.path.exists(workspace + "\\dem_raw_commonProjection.tif"):
-        arcpy.Project_management(dem, "dem_raw_commonProjection.tif", spatialRef)
+def commonCoords(glaciers, debris, spatialRef, workingFolder):
+    import arcpy, os
+    # case of defining a different projection for both glacier and debris shapefiles
+    if spatialRef != "glacier_shp" and spatialRef != "debris_shp" and spatialRef != "skip":
+        if not os.path.exists(workingFolder + "\\debris_raw_commonProjection.tif"):
+            arcpy.Project_management(debris, workingFolder +"debris_raw_commonProjection.tif", spatialRef)
+        if not os.path.exists(workingFolder + "\\glaciers_raw_commonProjection.shp"):
+            arcpy.Project_management(glaciers, workingFolder + "glaciers_raw_commonProjection.shp", spatialRef)
+    elif spatialRef == "glacier_shp":
+        spatialRef = arcpy.Describe(glaciers).spatialReference
+        if not os.path.exists(workingFolder + "\\glaciers_raw_commonProjection.tif"):
+            arcpy.CopyFeatures_management(glaciers, workingFolder + "\\glaciers_raw_commonProjection.tif")
+        if not os.path.exists(workingFolder + "\\debris_raw_commonProjection.shp"):
+            arcpy.Project_management(debris, workingFolder + "\\debris_raw_commonProjection.shp", spatialRef)
+    elif spatialRef == "debris_shp":
+        spatialRef = arcpy.Describe(debris).spatialReference
+        if not os.path.exists(workingFolder + "\\debris_raw_commonProjection.shp"):
+            arcpy.CopyFeatures_management(debris, workingFolder + "\\debris_raw_commonProjection.shp")
+        if not os.path.exists(workingFolder + "\\glaciers_raw_commonProjection.tif"):
+            arcpy.Project_management(glaciers, workingFolder + "\\glaciers_raw_commonProjection.tif", spatialRef)
+    elif spatialRef == "skip":
+        pass #dont't perform O(n) copy operation 
